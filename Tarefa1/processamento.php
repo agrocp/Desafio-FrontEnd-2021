4eb6eca58f1a6707c60f91c32f8291350d2df8b7
Heuller <heullergit@gmail.com>
2021-06-12 11:33:43 -0300

    front parcial e ja consumindo api

---
+++ Tarefa1/processamento.php
@@ -0,0 +1,12 @@
+<?php
+        $url = "https://viacep.com.br/ws/" . $_POST['cep'] . "/" . "json/";
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $resultado = json_decode(curl_exec($ch));
+
+        //var_dump($resultado);
+        header('Location: /AgroCP/Desafio-FrontEnd-2021/Tarefa1/');
+        
+    
+?>
